544e4a084c0b059e713853eccdcb4cdb97ffd895
Rodion Gorkovenko <RodionGork@github.com>
2015-10-27 22:45:17 +0400

    Added lights and ballast

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function addSleepers(scene) {
     var n = 21;
-    var m = new THREE.MeshBasicMaterial({color:0xA07000});
-    var g = new THREE.BoxGeometry(0.2, 0.1, 2);
+    var m = new THREE.MeshLambertMaterial({color:0xA07000});
+    var g = new THREE.BoxGeometry(0.4, 0.2, 2);
     for (var i = 0; i < n; i++) {
         var o = new THREE.Mesh(g, m);
         o.position.x = (i - (n - 1) / 2) * 1.0;
@@ -12,7 +12,7 @@ function addSleepers(scene) {
 function addRails(scene) {
     var s = 21;
     var n = 2;
-    var m = new THREE.MeshBasicMaterial({color:0x909090});
+    var m = new THREE.MeshLambertMaterial({color:0x909090});
     var g = new THREE.BoxGeometry(1.0, 0.2, 0.1);
     for (var j = 0; j < s; j++) {
         for (var i = 0; i < n; i++) {
@@ -22,6 +22,33 @@ function addRails(scene) {
             o.position.x = (j - (s - 1) / 2) * 1.0;
             scene.add(o);
         }
+    }
+}
+
+function addBallast(scene) {
+    var n = 21;
+    var m = new THREE.MeshLambertMaterial({color:0x505555});
+    var p1 = new THREE.PlaneGeometry(1, 2.1);
+    var p2 = new THREE.PlaneGeometry(1, 1.6);
+    for (var i = 0; i < n; i++) {
+        var x = (i - (n - 1) / 2) * 1.0;
+        var o1 = new THREE.Mesh(p1, m);
+        o1.position.y = -0.1;
+        o1.position.x = x;
+        o1.rotateX(-Math.PI / 2);
+        var o2 = new THREE.Mesh(p2, m);
+        o2.position.y = -0.5;
+        o2.position.z = -1.7;
+        o2.position.x = x;
+        o2.rotateX(-Math.PI * 2 / 3);
+        var o3 = new THREE.Mesh(p2, m);
+        o3.position.y = -0.5;
+        o3.position.z = 1.7;
+        o3.position.x = x;
+        o3.rotateX(-Math.PI / 3);
+        scene.add(o1);
+        scene.add(o2);
+        scene.add(o3);
     }
 }
 
@@ -40,8 +67,15 @@ try {
 }
 renderer.setSize(width, height);
 
+var light1 = new THREE.DirectionalLight(0xffffff, 1.0);
+light1.position.set(0, 1, 0.5, 0.5);
+scene.add(light1);
+var light2 = new THREE.AmbientLight(0x777777);
+scene.add(light2);
+
 addSleepers(scene);
 addRails(scene);
+addBallast(scene);
 /*
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
